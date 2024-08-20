bc827f0112a8b7e30f6097302fbeb82bdb2f383b
hovhannisyan2010 <hovhannisyanjora2010@gmail.com>
2024-08-20 21:29:52 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Card from "./Components/Card";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App() {  
+  
+  const [page,setPage] = useState(1)
+  const [pageNums,setPageNums]=useState([1,page,3,"..."])
+  const api_key = "api_key=9b702a6b89b0278738dab62417267c49";
+  const [populary, setPopulary] = useState([]);
+  useEffect(() => {
+    fetch(`https://api.themoviedb.org/3/movie/popular?${api_key}&page=${page}`)
+      .then((res) => res.json())
+      .then((res) => {
+        setPopulary(res.results)
+        setPageNums([res.total_pages])
+      }
+    );
+  }, [page]);
+  console.log(populary);
+  return <div className="App">
+    <div className="parent">
+    {
+      populary.map((e)=>{
+        return <Card films={e} key={e.id}/>
+      })
+    }
     </div>
-  );
+    <div className="pages">
+      
+      <button onClick={()=>{setPage(page-1)}}>{page-1}</button>
+      <button onClick={()=>{setPage(page)}}>{page}</button>
+      <button onClick={()=>{setPage(page+1)}}>{page+1}</button>
+      <h1>.</h1>
+      <h1>.</h1>
+      <h1>.</h1>
+      <button onClick={()=>{setPage(pageNums)}}>{pageNums}</button>
+      
+
+    </div>
+  </div>;
 }
 
 export default App;
+
+// let root = document.getElementById("root");
+// let popularURL = "https://api.themoviedb.org/3/movie/popular?" + api_key;
---
+++ src/Components/Card.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Card({films}) {
+  return (
+    <div className='card'>
+      <img src={"https://image.tmdb.org/t/p/original/"+films.poster_path} className='img'/>
+      <h1>{films.original_title}</h1>
+    </div>
+  )
+}
+
+export default Card
